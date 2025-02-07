2cf08ff98faaf8e7a4c9c6d31b210be67aba4e41
daviderosso-data <daviderosso.data@gmail.com>
2025-02-07 18:09:37 +0100

    Version 0.5 - impostazione di base

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,96 @@
-// Funzione per eseguire la ricerca
-function search() {
-    const category = document.getElementById('searchCategory').value;
-    const apiUrl = `https://openlibrary.org/subjects/${category}.json`;
+// definisco variabili e costanti
+let offset = 0;
+let selectedCategory = '';
+const searchButton = document.getElementById('searchButton');
+const searchBox = document.getElementById('searchBox');
+const resultsContainer = document.getElementById('result-container');
+const prevPage = document.getElementById('prevPage');
+const nextPage = document.getElementById('nextPage');
 
-    // Esegue la chiamata API
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            displayResults(data);
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
-}
-function displayResults(data){
-
-    console.log(data);
-    const bookListContainer = document.getElementById('bookList');
-
-    // creo una lista ordinata
-    const list = document.createElement('ol');
-
-    data.works.forEach(work => {
-        const resultItem = document.createElement('div');
-        resultItem.className = 'result-item';
-
-        // Creazione del contenuto del titolo
-        const titleDiv = document.createElement('div');
-        titleDiv.className = 'title';
-        titleDiv.textContent = `Title: ${work.title}`;
-
-        // Creazione del contenuto dell'autore
-        const authorDiv = document.createElement('div');
-        authorDiv.className = 'author';
-        const authors = work.authors.map(author => author.name).join(', ');
-        authorDiv.textContent = `Author: ${authors}`;
-
-        // Aggregazione dei contenuti nel singolo elemento di risultato
-        resultItem.appendChild(titleDiv);
-        resultItem.appendChild(authorDiv);
-
-        // Aggiunta dell'elemento al contenitore dei risultati
-        resultsContainer.appendChild(resultItem);
-    });
-
-    // Aggiungi la lista al contenitore
-    bookListContainer.appendChild(list);
-}
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.autocomplete');
-    var instances = M.Autocomplete.init(elems, {
+//Inizializzo l'autocomplete della searchBox
+document.addEventListener('DOMContentLoaded', function(){
+    let elems = document.querySelectorAll('.autocomplete');
+    M.Autocomplete.init(elems, {
         data: {
-            "Apple": null,
-            "Banana": null,
-            "Cherry": null,
-            "Date": null,
-            "Elderberry": null,
-            "Fig": null,
-            "Grape": null
+            "fantasy": null,
+            "science": null
+        },
+        onAutocomplete: function(val) {
+            selectedCategory = val; 
         }
     });
+});
+
+
+//Funzione di ricerca
+function fetchBooks(category,offset){
+    const url = `https://openlibrary.org/subjects/${category}.json?offset=${offset}`;
+    let maxLength = 20;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data =>{
+            resultsContainer.innerHTML = '';
+            if (data.works.lenght === 0 ){
+                resultsContainer.innerHTML = '<p>Nessun Libro Trovato in questa categoria</p>';
+                return;
+            }
+            data.works.forEach(work => {
+                console.log(work
+                );
+                
+                
+                const card = document.createElement('div');
+                card.className = 'card';
+
+                const cardimage = document.createElement('div');
+                cardimage.className = 'card-image';
+
+
+                const title = document.createElement('span');
+                title.className = 'card-title';
+                title.textContent = work.title.length > 25 ? work.title.substring(0, 25) + "..." : work.title;
+
+
+                const cover = document.createElement('img');
+                cover.src =  `https://covers.openlibrary.org/b/id/${work.cover_id}.jpg`;
+
+                const button = document.createElement('button');
+                button.className = "custumBtn btn waves-effect waves-light";
+                button.textContent = "descrizione";
+
+                const author = document.createElement('p');
+                author.className = 'card-author';
+                author.textContent = work.authors[0].name.length > 20 ? work.authors[0].name.substring(0, 20) + "..." : work.authors[0].name;
+                
+                card.appendChild(title);
+                card.appendChild(author);
+                cardimage.appendChild(cover);
+                card.appendChild(cardimage);
+                cardimage.appendChild(button);
+                
+                
+                resultsContainer.appendChild(card);
+
+                prevPage.disabled = offset === 0;
+            });
+        });
+
+}
+
+searchButton.addEventListener('click', () =>{
+    offset = 0;
+    fetchBooks(selectedCategory, offset);
+});
+
+nextPage.addEventListener('click', ()=>{
+    offset += 12;
+    fetchBooks(selectedCategory, offset);
+});
+
+prevPage.addEventListener('click', ()=>{
+    if (offset > 0){
+        offset -= 12;
+        fetchBooks(selectedCategory, offset);
+    }
 });
